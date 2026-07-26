bbce83e745ace9dafd20ef4effd929b315c20794
JonasDevOne <jonas.knopf06@outlook.de>
2026-07-26 19:59:19 +0200

    Small changes and refinements

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,4 +1,6 @@
 export const baseData = {
   title: "Glücksknopf",
-  description: "Willkommen zu meiner Webseite!"
+  description: "Herzlich willkommen auf meiner Webseite!",
+  about: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore sed veritatis quaerat repellendus necessitatibus soluta, " +
+    "numquam voluptate, architecto quasi perspiciatis maxime. Possimus perferendis consequuntur ipsum molestias asperiores harum, eius ad!"
 };
