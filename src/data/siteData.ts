18f8ac6aeec566d001cf32251ba1cfa8bbd0a64d
JonasDevOne <jonas.knopf06@outlook.de>
2026-09-02 02:51:30 +0200

    Modal and Navigation

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -2,5 +2,6 @@ export const baseData = {
   title: "Glücksknopf",
   description: "Herzlich willkommen auf meiner Webseite!",
   about: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore sed veritatis quaerat repellendus necessitatibus soluta, " +
-    "numquam voluptate, architecto quasi perspiciatis maxime. Possimus perferendis consequuntur ipsum molestias asperiores harum, eius ad!"
+    "numquam voluptate, architecto quasi perspiciatis maxime. Possimus perferendis consequuntur ipsum molestias asperiores harum, eius ad! " +
+    "numquam voluptate, architecto quasi perspiciatis maxime. Possimus perferendis consequuntur ipsum molestias asperiores harum, eius ad! "
 };
