88438813413f8578669d246577ecaa665b5e03eb
JonasDevOne <jonas.knopf06@outlook.de>
2026-09-09 16:03:53 +0200

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: 'https://jonasdevone.github.io',
+  base: '/gluecksknopf-website',
 });
